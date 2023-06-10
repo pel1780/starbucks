e9699bdfcab2a5d57f5a54506cba10e8ec5ccaf5
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-06-10 18:39:27 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,39 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeHolder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 300) {
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll(".mainVisual .fade_in");
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+new Swiper(".notice_line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
